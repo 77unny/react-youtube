6de4ebc7b677539eab8cf952404a27171cf6eec8
baekCode <baekcode@gmail.com>
2020-10-02 11:03:38 +0900

    feat : router skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect
+} from "react-router-dom";
+
+import Home from "./router/Home";
+import Detail from "./router/Detail";
 
 function App() {
 	return (
-		<div>dd</div>
+			<Router>
+				<div>Header components</div>
+				<Switch>
+					<Route exact path="/" component={Home}/>
+					<Route path="/detail/:id" component={Detail}/>
+					<Redirect path="*" to="/" />
+				</Switch>
+			</Router>
 	);
 }
 
 export default App;
+
+/*
+* API KEY : process.env.REACT_APP_API_KEY
+* search API : `${process.env.REACT_APP_SEARCH}?part=snippet&maxResults=25&q=${keyword}&key=${process.env.REACT_APP_API_KEY}`
+* popular API : `${process.env.REACT_APP_POPULAR}?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_API_KEY}`
+* */
